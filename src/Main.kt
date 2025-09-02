abffbeb16485d032457c032a9b614b3de50e6b64
Leonardo Pereyra <leonardopereyra@MacBook-Air-de-Leonardo.local>
2025-09-02 13:45:58 -0600

    Private Constructor Example

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    InitOrderDemo(Person("leo", "pereyra"))
+    /**   InitOrderDemo(Person("leo", "pereyra"))
     val person = PersonConstructor(name = "Leo")
     val person2 = PersonConstructor(name = "Leo", lastName = "Pereyra")
     println("Persona 1 name ${person.name}, age = ${person.age},lastName = ${person.lastName}")
     println("Persona 2 name ${person2.name}, lastname ${person2.lastName}")
+
+    //Private Constructor
+    val dontCreateConstructorOut: DontCreateMe? = null
+    val dontCreateWithConstructor = DontCreateMe(name = "Leo", lastName = "Pereyra")
+
+
+    User.createWithEmail("leo","mail@.com").also {println(it.toString())  }
+     **/
 
 }
